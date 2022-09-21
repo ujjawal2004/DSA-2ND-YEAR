441b4ca138962e8d91fab81e1fe55273f3ce7c64
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2022-09-21 23:34:42 +0530

    Create basicdatatypes.cpp

---
+++ Assignment/Assignment 1/basicdatatypes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+     long b;
+     
+     
+     char d;
+     float e;
+     double f;
+     
+     scanf("%d %ld %c %f %lf", &a, &b, &d, &e, &f);
+     printf("%d\n", a);
+     printf("%ld\n", b);
+     
+     printf("%c\n", d);
+     printf("%f\n", e);
+     printf("%lf", f);
+     
+     /* cin>>a>>b>>d>>e>>f;
+      cout<<a<<"\n";
+      cout<<b<<"\n";
+      cout<<d<<"\n";
+      cout<<e<<"\n";
+      cout<<f<<"\n";
+    */
+    
+      return 0;
+    
+}
