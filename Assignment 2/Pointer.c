ca6070f32e96c7841a3c2826700a65f0cb47014b
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2022-09-25 22:04:51 +0530

    Create Pointer.c

---
+++ Assignment 2/Pointer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function  
+    int sum,diff;
+     sum = *a+*b;
+     diff = abs(*a-*b);
+     *a = sum;
+     *b = diff;  
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+https://www.hackerrank.com/challenges/pointer-in-c/submissions/code/292517165
