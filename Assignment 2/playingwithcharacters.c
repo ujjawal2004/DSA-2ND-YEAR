be8de3f4170acd84bba5082f0251984fe658ebc5
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2022-09-25 21:54:14 +0530

    Create playingwithcharacters.c

---
+++ Assignment 2/playingwithcharacters.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    char ch;
+    char s[24];
+    char sen[100];
+    scanf("%c\n%s\n%[^\n]s",&ch,s,sen);
+    printf("%c\n%s\n%s",ch,s,sen);  
+    return 0;
+}
+
+https://www.hackerrank.com/challenges/playing-with-characters/submissions/code/292513268
