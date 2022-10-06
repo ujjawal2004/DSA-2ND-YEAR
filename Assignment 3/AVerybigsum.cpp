c65e2762a2da218a2141778092ff89d39d674e42
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2022-10-06 23:07:01 +0530

    Create AVerybigsum.cpp

---
+++ Assignment 3/AVerybigsum.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+int n;
+long long int sum=0;
+
+cin >> n;
+
+vector<int> arr(n);
+
+for(int arr_i = 0;arr_i < n;arr_i++)
+{
+cin >> arr[arr_i];
+sum+=arr[arr_i];
+}
+
+cout<<sum;
+return 0;
+
+}
+
+https://www.hackerrank.com/challenges/a-very-big-sum/submissions/code/294505796
