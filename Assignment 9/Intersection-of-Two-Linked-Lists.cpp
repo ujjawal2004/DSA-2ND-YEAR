4652d2a30704689d2fb987969c0ab54040ba6423
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2023-01-15 20:37:55 +0530

    Create Intersection-of-Two-Linked-Lists.cpp

---
+++ Assignment 9/Intersection-of-Two-Linked-Lists.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		unordered_map<ListNode*, int> m;
+		while(headA != NULL){
+			m[headA]++;
+			headA = headA -> next;
+		}
+		while(headB != NULL){
+			if(m[headB] > 0){
+				return headB;
+			}
+			headB = headB -> next;
+		}
+		return NULL;
+	}
+};
