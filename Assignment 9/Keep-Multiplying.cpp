b5ea08a82484c1106a620cf2a4d9b6b9f7c9a917
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2023-01-15 20:46:44 +0530

    Create Keep-Multiplying.cpp

---
+++ Assignment 9/Keep-Multiplying.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int o) {
+        int h[1001]={};
+        for(auto i:nums) h[i]++;
+		
+        while(o<=1000 && h[o])
+            o*=2;
+        
+        return o;
+    }
+};
