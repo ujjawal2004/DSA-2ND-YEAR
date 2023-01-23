8908ab9cd85e8bc5293c369f3175b0ce49f84aa2
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2023-01-23 09:43:51 +0530

    Create inorder-hackerrank.cpp

---
+++ Assignment(tree)/inorder-hackerrank.cpp
@@ -0,0 +1,29 @@
+
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void inOrder(Node *root) {
+        if(root == NULL){
+        return;
+    }
+    inOrder(root->left);
+    printf("%d ", root->data);
+    inOrder(root->right);
+
+    }
+
