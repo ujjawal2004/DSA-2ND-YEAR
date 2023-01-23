e6ba41e76c5145abd05441cb75edb196e40b1565
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2023-01-23 09:39:36 +0530

    Create postorder-leetcode.cpp

---
+++ Assignment(tree)/postorder-leetcode.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int>res;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root != NULL)
+        {
+            postorderTraversal(root->left);
+            postorderTraversal(root->right);
+            res.push_back(root->val);
+        }
+        return res;
+    }
+};
