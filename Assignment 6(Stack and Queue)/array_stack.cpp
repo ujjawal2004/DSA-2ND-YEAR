45d05c7d24744637e904b2251a4396a1ebe87558
ujjawal2004 <107607581+ujjawal2004@users.noreply.github.com>
2023-01-30 21:39:44 +0530

    Create array_stack.cpp

---
+++ Assignment 6(Stack and Queue)/array_stack.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+ 
+//complete the functions
+class Stack{
+    public:
+	int A[100000];
+	int end;
+ 
+	public:
+	//constructor is called whenever an object of the class is declared
+	Stack(){
+		cout<<"constructor is Called\n";
+		end=0;
+	}
+ 
+	//push the value at the back
+	void push(int val){
+		cout<<"pushing the val: "<<val<<" at the back of queue\n";
+       A[end]=val;
+        end++;
+	}
+ 
+	int top(){
+		cout<<"returning the value at top or last\n";
+		return A[end-1];
+	}
+ 
+	void pop(){
+		cout<<"removing the last element\n";
+        end--;
+	}
+ 
+	int size(){
+		cout<<"return the size of the queue\n";
+		return end;
+	}
+ 
+	bool isEmpty(){
+		cout<<"returning if the stack is empty\n";
+		return true;
+	}
+};
+ 
+int main() {
+	Stack s; //constructor is called on this line
+	s.push(4);
+	cout<<"top: "<<s.top()<<"\n";
+	s.pop();
+	cout<<"size: "<<s.size()<<"\n";
+	cout<<"empty: "<<s.isEmpty()<<"\n";
+ 
+	s.push(5);
+	s.push(3);
+	cout<<"front: "<<s.top()<<"\n";
+	s.pop();
+	cout<<"size: "<<s.size()<<"\n";
+	cout<<"empty: "<<s.isEmpty()<<"\n";
+ 
+	cout<<"top: "<<s.top()<<"\n";
+	s.pop();
+	cout<<"size: "<<s.size()<<"\n";
+	cout<<"empty: "<<s.isEmpty()<<"\n";
+	return 0;
+}
